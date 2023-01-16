bc0f47dee987f9288c14c992eee87f1b730741b9
zhangchuang <keikeizhang540623@gmail.com>
2023-01-16 08:39:44 +0800

    修改 vite-config 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-// https://vitejs.dev/config/
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
+}
+
 export default defineConfig({
   plugins: [vue()],
   server: {
@@ -9,7 +13,10 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      {
+        find: '@',
+        replacement: pathResolve('src')
+      }
     }
   }
 })
